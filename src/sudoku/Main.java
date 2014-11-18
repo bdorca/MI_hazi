bf95cc146fe6fdc12049842c627e74c1d6b93201
Legoocska <mayer.emil@gmail.com>
2014-11-18 10:31:39 +0100

    sudokuu

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,6 +1,32 @@
 package sudoku;
 //van-e ebben main?
 //yay
+
+
+
+//SSSSSSSSSSSSSSS UUUUUUUU     UUUUUUUUDDDDDDDDDDDDD             OOOOOOOOO     KKKKKKKKK    KKKKKKKUUUUUUUU     UUUUUUUU
+//SS:::::::::::::::SU::::::U     U::::::UD::::::::::::DDD        OO:::::::::OO   K:::::::K    K:::::KU::::::U     U::::::U
+//S:::::SSSSSS::::::SU::::::U     U::::::UD:::::::::::::::DD    OO:::::::::::::OO K:::::::K    K:::::KU::::::U     U::::::U
+//S:::::S     SSSSSSS U:::::U     U:::::UUDDD:::::DDDDD:::::D  O:::::::OOO:::::::OK:::::::K   K::::::KUU:::::U     U:::::UU
+//S:::::S             U:::::U     U:::::U   D:::::D    D:::::D O::::::O   O::::::OKK::::::K  K:::::KKK U:::::U     U:::::U 
+//S:::::S             U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::K K:::::K    U:::::D     D:::::U 
+//S::::SSSS          U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K::::::K:::::K     U:::::D     D:::::U 
+//SS::::::SSSSS     U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::::::::K      U:::::D     D:::::U 
+// SSS::::::::SS   U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::::::::K      U:::::D     D:::::U 
+//    SSSSSS::::S  U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K::::::K:::::K     U:::::D     D:::::U 
+//         S:::::S U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::K K:::::K    U:::::D     D:::::U 
+//         S:::::S U::::::U   U::::::U   D:::::D    D:::::D O::::::O   O::::::OKK::::::K  K:::::KKK U::::::U   U::::::U 
+//SSSSSSS     S:::::S U:::::::UUU:::::::U DDD:::::DDDDD:::::D  O:::::::OOO:::::::OK:::::::K   K::::::K U:::::::UUU:::::::U 
+//S::::::SSSSSS:::::S  UU:::::::::::::UU  D:::::::::::::::DD    OO:::::::::::::OO K:::::::K    K:::::K  UU:::::::::::::UU  
+//S:::::::::::::::SS     UU:::::::::UU    D::::::::::::DDD        OO:::::::::OO   K:::::::K    K:::::K    UU:::::::::UU    
+//SSSSSSSSSSSSSSS         UUUUUUUUU      DDDDDDDDDDDDD             OOOOOOOOO     KKKKKKKKK    KKKKKKK      UUUUUUUUU      
+                                                                                                                      
+                                                                                                                      
+                                                                                                                      
+                                                                                                                      
+                                                                                                                      
+                                                                                                                      
+                                                                                                                      
 public class Main {
 
 }
