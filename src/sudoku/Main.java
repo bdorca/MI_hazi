91cedecf8ab40cb6dffbf9ca9f8cf60f3e227562
bdorca <bdorca@gmail.com>
2014-11-22 22:42:52 +0100

    Emil fantasztikus megoldója

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -4,58 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-//van-e ebben main?
-//yay
 import java.util.Random;
 
-
-
-//SSSSSSSSSSSSSSS UUUUUUUU     UUUUUUUUDDDDDDDDDDDDD             OOOOOOOOO     KKKKKKKKK    KKKKKKKUUUUUUUU     UUUUUUUU
-//SS:::::::::::::::SU::::::U     U::::::UD::::::::::::DDD        OO:::::::::OO   K:::::::K    K:::::KU::::::U     U::::::U
-//S:::::SSSSSS::::::SU::::::U     U::::::UD:::::::::::::::DD    OO:::::::::::::OO K:::::::K    K:::::KU::::::U     U::::::U
-//S:::::S     SSSSSSS U:::::U     U:::::UUDDD:::::DDDDD:::::D  O:::::::OOO:::::::OK:::::::K   K::::::KUU:::::U     U:::::UU
-//S:::::S             U:::::U     U:::::U   D:::::D    D:::::D O::::::O   O::::::OKK::::::K  K:::::KKK U:::::U     U:::::U 
-//S:::::S             U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::K K:::::K    U:::::D     D:::::U 
-//S::::SSSS          U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K::::::K:::::K     U:::::D     D:::::U 
-//SS::::::SSSSS     U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::::::::K      U:::::D     D:::::U 
-// SSS::::::::SS   U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::::::::K      U:::::D     D:::::U 
-//    SSSSSS::::S  U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K::::::K:::::K     U:::::D     D:::::U 
-//         S:::::S U:::::D     D:::::U   D:::::D     D:::::DO:::::O     O:::::O  K:::::K K:::::K    U:::::D     D:::::U 
-//         S:::::S U::::::U   U::::::U   D:::::D    D:::::D O::::::O   O::::::OKK::::::K  K:::::KKK U::::::U   U::::::U 
-//SSSSSSS     S:::::S U:::::::UUU:::::::U DDD:::::DDDDD:::::D  O:::::::OOO:::::::OK:::::::K   K::::::K U:::::::UUU::::::U 
-//S::::::SSSSSS:::::S  UU:::::::::::::UU  D:::::::::::::::DD    OO:::::::::::::OO K:::::::K    K:::::K  UU:::::::::::::UU  
-//S:::::::::::::::SS     UU:::::::::UU    D::::::::::::DDD        OO:::::::::OO   K:::::::K    K:::::K    UU:::::::::UU    
-//SSSSSSSSSSSSSSS         UUUUUUUUU      DDDDDDDDDDDDD             OOOOOOOOO     KKKKKKKKK    KKKKKKK      UUUUUUUUU      
-                                                                                                                      
-                                                                                                                      
-                                                                                                                      
-                                                                                                                      
-
-
-
-
-                                                                                                                      
-                                                                                                                      
 public class Main {
 
-	
-	
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		int nehezseg = 6;
 		FileReader fr;
 		try {
-			
-			//fr = new FileReader("C:\\Users\\Legoo\\Desktop\\f�l�v#5\\MI\\sudoku\\MI_hazi\\src\\9char.txt");
-			fr = new FileReader("9char.txt");
+
+			//fr = new FileReader("C:\\Users\\Legoo\\Desktop\\félév#5\\MI\\sudoku\\MI_hazi\\src\\9char.txt");
+			fr= new FileReader("9char.txt");
 			BufferedReader br = new BufferedReader(fr);
-			Random rand= new Random();
-			int sorszam=rand.nextInt(1758);
-			for(int i=0;i<sorszam;i++){
+			Random rand = new Random();
+			int sorszam = rand.nextInt(1758);
+			for (int i = 0; i < sorszam; i++) {
 				br.readLine();
 			}
-			String szo=br.readLine();
+			String szo = br.readLine();
 			System.out.println(szo);
-			Tabla.karakterkeszlet=szo.toCharArray();
-			
+			Tabla.karakterkeszlet = szo.toCharArray();
+
 			br.close();
 			fr.close();
 		} catch (FileNotFoundException e) {
@@ -65,36 +34,41 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Tabla t=new Tabla();
-		
+		Tabla t = new Tabla();
+
 		t.ToltPuzzle(t.puzzle);
-		t.makePuzzle(t.puzzle, 0);
-		
-		int[] tomb=new int[81];
-		for(int i=0;i<81;i++){
-			tomb[i]=t.puzzle[i];
+		while (!(t.makePuzzle(t.puzzle, 0))) {
+			for (int i = 0; i < 10; i++) {
+				System.out
+						.println("HEUREEEEEEEEEEEEEEEEEEEEEKAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!!!!!!!!44");
+			}
+			t.szam = 0;
+			t.ToltPuzzle(t.puzzle);
+
 		}
-		
-		for(int i = 0; i < 40; i++){ //Az, hogy h�ny sz�mot t�vol�tsunk el (ha mindig m�st gener�l a k�t random)
+		// t.makePuzzle(t.puzzle, 0);
+
+		for (int i = 0; i < (nehezseg * 10 + 1); i++) { // Az, hogy hány számot
+														// távolítsunk el (ha
+														// mindig mást generál a
+														// két random)
 			Random rand = new Random();
 			int value = rand.nextInt(9);
 			int value_2 = rand.nextInt(9);
-			t.puzzle[value*9+value_2] = 0;
-		}
-		
-		System.out.println("\n" + t.toString() + "\n");
-		t.szam = 0;
-		t.makePuzzle(t.puzzle, 0);
-		
-		boolean zsir=true;
-		for(int i=0;i<81;i++){
-			if(tomb[i]!=t.puzzle[i]){
-				zsir=false;
+			while (t.puzzle[value * 9 + value_2] == 0) {
+				value = rand.nextInt(9);
+				value_2 = rand.nextInt(9);
 			}
+			t.puzzle[value * 9 + value_2] = 0;
+
 		}
-		if(zsir){
-			System.out.println("ZSIR");
-		}
-		
-	}//Main f�ggv�ny v�ge
-}//Main v�ge
+
+		System.out.println("\n" + t.toString() + "\n");
+		// t.szam = 0;
+		// t.save81();
+		System.out.println("A megoldas:");
+		t.solvePuzzle(t.puzzle, 0);
+
+	}// Main fï¿½ggvï¿½ny vï¿½ge
+}// Main vï¿½ge
+
--- src/sudoku/Tabla.java
+++ src/sudoku/Tabla.java
@@ -3,29 +3,43 @@ package sudoku;
 import java.util.Random;
 
 public class Tabla {
-    public int[] puzzle;
-    public int szam = 0;
-    public char[] chpuzzle;
-    public static char[] karakterkeszlet;
-    
+	public int[] puzzle;
+	public int[] nyolcvanegy;
+	public int szamlalo = 0;
+	public int szam = 0;
+	public char[] chpuzzle;
+	public static char[] karakterkeszlet;
+
 	public Tabla() {
-	    puzzle = new int[81];
+		puzzle = new int[81];
+		nyolcvanegy = new int[81];
 	}
-	
-	
-	public boolean makePuzzle(int[] puzzle, int i)
-    {
+
+	public void save81() {
+		for (int i = 0; i < 81; i++) {
+			nyolcvanegy[i] = puzzle[i];
+		}
+	}
+
+	public void load81() {
+		for (int i = 0; i < 81; i++) {
+			puzzle[i] = nyolcvanegy[i];
+		}
+	}
+
+	public boolean makePuzzle(int[] puzzle, int i) {
 		szam++;
 		System.out.println(szam);
-		if(szam>50000){ 
+		if (szam > 1000) {
 			System.out.println("shit.");
 			return false;
-		} 
-        for (int x = 1; x< 10 ; x++)
-        {
+		}
+
+		for (int x = 1; x < 10; x++) {
 			if (puzzle[i] != 0) {
 				if (makePuzzle(puzzle, i + 1))
 					return true;
+
 			} else {
 				puzzle[i] = x;
 				if (checkConstraints(puzzle)) {
@@ -38,82 +52,303 @@ public class Tabla {
 						return true;
 					} else if (makePuzzle(puzzle, i + 1))
 						return true;// find a number for the next square
+
 				}
 				puzzle[i] = 0;// this try didn't work, delete the evidence
 			}
-        }
-        return false;
-        
-    }
-    public boolean checkConstraints(int[] puzzle)
-    {
-        int test;
-        
-      for(int sor = 0; sor < 9; sor++){
-    	  test = puzzle[sor*10];
-    	  for(int oszlop = 0; oszlop < 9; oszlop++){
-        	  if(test != 0 && oszlop != sor && test == puzzle[oszlop*10])
-        		  return false;
-    	  }
-      }
-        
-     //test that rows have unique values    
-      for (int column=0; column<9; column++)
-        {
-            for (int row=0; row<9; row++)
-            {
-                test=puzzle[row+column*9];
-                for (int j=0;j<9;j++)
-                {
-                    if( test!=0 &&  row!=j && test==puzzle[j+column*9])
-                        return false;
-                }
-            }
-        }
-        //test that columns have unique values
-        for  (int column=0; column<9; column++) 
-        {
-             for(int row=0; row<9; row++)
-            {
-                test=puzzle[column+row*9];
-                for (int j=0;j<9;j++)
-                {
-                    if(test!=0 && row!=j && test==puzzle[column+j*9])
-                        return false;
-                }
-            }
-        }
-        //implement region test here
-        /*
-        int[][] regions = new int[9][9];
-        int[] regionIndex ={0,3,6,27,30,33,54,57,60};
-        for (int region=0; region<9;region++) //for each region
-        {
+		}
+		return false;
+	}
 
-            int j =0;
-            for (int k=regionIndex[region];k<regionIndex[region]+27; k=(k%3==2?k+7:k+1))
-                {
-                    regions[region][j]=puzzle[k];
-                    j++;
-                }
-        }
-        for (int i=0;i<9;i++)//region counter
-        {
-            for (int j=0;j<9;j++)
-            {
-                for (int k=0;k<9;k++)
-                {
-                    if (regions[i][j]!=0&&j!=k&&regions[i][j]==regions[i][k])
-                    return false;
-                }
+	public boolean solvePuzzle(int[] puzzle, int i) {
 
-            }
-        }*/
-        
-        for (int i=0;i<9;i++)//region counter
-        {
-            for (int j=0;j<9;j++)
-            {
+		boolean check = true;
+
+		for (int j = 0; j < 81; j++) {
+			if (puzzle[j] == 0)
+				check = false;
+		}
+
+		if (check) {
+			System.out.println(this);
+			return true;
+		}
+
+		if (puzzle[i] != 0) {
+			if (i < 80) {
+				if (solvePuzzle(puzzle, i + 1))
+					return true;// find a number for the next square
+			} else {
+				if (solvePuzzle(puzzle, 0))
+					return true;// find a number for the next square
+			}
+		} else if (!(checkSquare(puzzle, i))) {
+			if (i < 80) {
+				if (solvePuzzle(puzzle, i + 1))
+					return true;// find a number for the next square
+			} else {
+				if (solvePuzzle(puzzle, 0))
+					return true;// find a number for the next square
+			}
+		} else {
+
+			for (int x = 1; x < 10; x++) {
+
+				puzzle[i] = x;
+				if (checkConstraints(puzzle)) {
+					if (i < 80) {
+						if (solvePuzzle(puzzle, i + 1))
+							return true;// find a number for the next square
+					} else {
+						if (solvePuzzle(puzzle, 0))
+							return true;// find a number for the next square
+					}
+				}
+				puzzle[i] = 0;// this try didn't work, delete the evidence
+			}
+		}
+		return false;
+	}
+
+	public boolean checkSquare(int[] puzzle, int i) {
+		int sor = i / 9;
+		int oszlop = i % 9;
+		int[] kilenc = new int[9];
+		for (int j = 0; j < 9; j++) {
+			kilenc[j] = j + 1;
+		}
+
+		for (int j = 0; j < 9; j++) {
+			if (puzzle[sor * 9 + j] != 0) {
+				kilenc[puzzle[sor * 9 + j] - 1] = -1;
+			}
+		}
+		for (int j = 0; j < 9; j++) {
+			if (puzzle[j * 9 + oszlop] != 0) {
+				kilenc[puzzle[j * 9 + oszlop] - 1] = -1;
+			}
+		}
+
+		switch (i) {
+		case 0:
+		case 1:
+		case 2:
+		case 9:
+		case 10:
+		case 11:
+		case 18:
+		case 19:
+		case 20:
+
+			for (int k = 0; k < 3; k++) {
+				for (int z = 0; z < 3; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+
+		case 3:
+		case 4:
+		case 5:
+		case 12:
+		case 13:
+		case 14:
+		case 21:
+		case 22:
+		case 23:
+
+			for (int k = 0; k < 3; k++) {
+				for (int z = 3; z < 6; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+
+		case 6:
+		case 7:
+		case 8:
+		case 15:
+		case 16:
+		case 17:
+		case 24:
+		case 25:
+		case 26:
+
+			for (int k = 0; k < 3; k++) {
+				for (int z = 6; z < 9; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+
+		case 27:
+		case 28:
+		case 29:
+		case 36:
+		case 37:
+		case 38:
+		case 45:
+		case 46:
+		case 47:
+
+			for (int k = 3; k < 6; k++) {
+				for (int z = 0; z < 3; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		case 30:
+		case 31:
+		case 32:
+		case 39:
+		case 40:
+		case 41:
+		case 48:
+		case 49:
+		case 50:
+
+			for (int k = 3; k < 6; k++) {
+				for (int z = 3; z < 6; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		case 33:
+		case 34:
+		case 35:
+		case 42:
+		case 43:
+		case 44:
+		case 51:
+		case 52:
+		case 53:
+
+			for (int k = 3; k < 6; k++) {
+				for (int z = 6; z < 9; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		case 54:
+		case 55:
+		case 56:
+		case 63:
+		case 64:
+		case 65:
+		case 72:
+		case 73:
+		case 74:
+
+			for (int k = 6; k < 9; k++) {
+				for (int z = 0; z < 3; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		case 57:
+		case 58:
+		case 59:
+		case 66:
+		case 67:
+		case 68:
+		case 75:
+		case 76:
+		case 77:
+
+			for (int k = 6; k < 9; k++) {
+				for (int z = 3; z < 6; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		case 60:
+		case 61:
+		case 62:
+		case 69:
+		case 70:
+		case 71:
+		case 78:
+		case 79:
+		case 80:
+
+			for (int k = 6; k < 9; k++) {
+				for (int z = 6; z < 9; z++) {
+					if (puzzle[k * 9 + z] != 0)
+						kilenc[puzzle[k * 9 + z] - 1] = -1;
+				}
+			}
+			break;
+		}
+
+		int szam = 0;
+		for (int j = 0; j < 9; j++) {
+			if (kilenc[j] == -1)
+				szam++;
+		}
+		if (szam < 6)
+			return false;
+
+		return true;
+	}
+
+	public boolean checkConstraints(int[] puzzle) {
+		int test;
+
+		for (int sor = 0; sor < 9; sor++) {
+			test = puzzle[sor * 10];
+			for (int oszlop = 0; oszlop < 9; oszlop++) {
+				if (test != 0 && oszlop != sor && test == puzzle[oszlop * 10])
+					return false;
+			}
+		}
+
+		// test that rows have unique values
+		for (int column = 0; column < 9; column++) {
+			for (int row = 0; row < 9; row++) {
+				test = puzzle[row + column * 9];
+				for (int j = 0; j < 9; j++) {
+					if (test != 0 && row != j && test == puzzle[j + column * 9])
+						return false;
+				}
+			}
+		}
+		// test that columns have unique values
+		for (int column = 0; column < 9; column++) {
+			for (int row = 0; row < 9; row++) {
+				test = puzzle[column + row * 9];
+				for (int j = 0; j < 9; j++) {
+					if (test != 0 && row != j && test == puzzle[column + j * 9])
+						return false;
+				}
+			}
+		}
+		// implement region test here
+		/*
+		 * int[][] regions = new int[9][9]; int[] regionIndex
+		 * ={0,3,6,27,30,33,54,57,60}; for (int region=0; region<9;region++)
+		 * //for each region {
+		 * 
+		 * int j =0; for (int k=regionIndex[region];k<regionIndex[region]+27;
+		 * k=(k%3==2?k+7:k+1)) { regions[region][j]=puzzle[k]; j++; } } for (int
+		 * i=0;i<9;i++)//region counter { for (int j=0;j<9;j++) { for (int
+		 * k=0;k<9;k++) { if
+		 * (regions[i][j]!=0&&j!=k&&regions[i][j]==regions[i][k]) return false;
+		 * }
+		 * 
+		 * } }
+		 */
+
+		for (int i = 0; i < 9; i++)// region counter
+		{
+			for (int j = 0; j < 9; j++) {
 				int ertek = i * 9 + j;
 				test = puzzle[i * 9 + j];
 
@@ -121,178 +356,192 @@ public class Tabla {
 				case 0: case 1: case 2:
 				case 9: case 10: case 11:
 				case 18: case 19: case 20:
-					
+
 					for (int k = 0; k < 3; k++) {
 						for (int l = 0; l < 3; l++) {
-							if (test != 0 && ((k * 9 + l) != (i * 9 + j)) && (test == puzzle[k * 9 + l]))
+							if (test != 0 && ((k * 9 + l) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + l]))
 								return false;
 						}
 					}
 					break;
-				
-				case 3: case 4: case 5:
-				case 12: case 13: case 14:
-				case 21: case 22: case 23:
-					
+
+                case 3: case 4: case 5:
+                case 12: case 13: case 14:
+                case 21: case 22: case 23:
+ 
 					for (int k = 0; k < 3; k++) {
 						for (int z = 3; z < 6; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE");
 								return false;
-								}
+							}
 						}
 					}
 					break;
-					
-				case 6: case 7: case 8:
-				case 15: case 16: case 17:
-				case 24: case 25: case 26:
-					
+
+                    
+             case 6: case 7: case 8:
+             case 15: case 16: case 17:
+             case 24: case 25: case 26:
+
 					for (int k = 0; k < 3; k++) {
 						for (int z = 6; z < 9; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_2");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_2");
 								return false;
-								}
+							}
 						}
 					}
 					break;
-				
-				case 27: case 28: case 29:
-				case 36: case 37: case 38:
-				case 45: case 46: case 47:
-					
+
+             case 27: case 28: case 29:
+             case 36: case 37: case 38:
+             case 45: case 46: case 47:
+
 					for (int k = 3; k < 6; k++) {
 						for (int z = 0; z < 3; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_3");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_3");
 								return false;
-								}
+							}
 						}
 					}
 					break;
-				case 30: case 31: case 32:
-				case 39: case 40: case 41:
-				case 48: case 49: case 50:
-					
+
+             case 30: case 31: case 32:
+             case 39: case 40: case 41:
+             case 48: case 49: case 50:
+
 					for (int k = 3; k < 6; k++) {
 						for (int z = 3; z < 6; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_4");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_4");
 								return false;
-								}
+							}
 						}
 					}
 					break;
-				case 33: case 34: case 35:
-				case 42: case 43: case 44:
-				case 51: case 52: case 53:
-					
+
+             case 33: case 34: case 35:
+             case 42: case 43: case 44:
+             case 51: case 52: case 53:
+
 					for (int k = 3; k < 6; k++) {
 						for (int z = 6; z < 9; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_5");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_5");
 								return false;
-								}
+							}
 						}
 					}
 					break;
 				case 54: case 55: case 56:
 				case 63: case 64: case 65:
 				case 72: case 73: case 74:
-					
+
 					for (int k = 6; k < 9; k++) {
 						for (int z = 0; z < 3; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_6");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_6");
 								return false;
-								}
+							}
 						}
 					}
 					break;
 				case 57: case 58: case 59:
 				case 66: case 67: case 68:
 				case 75: case 76: case 77:
-					
+
 					for (int k = 6; k < 9; k++) {
 						for (int z = 3; z < 6; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_6");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_6");
 								return false;
-								}
+							}
 						}
 					}
 					break;
 				case 60: case 61: case 62:
 				case 69: case 70: case 71:
 				case 78: case 79: case 80:
-					
+
 					for (int k = 6; k < 9; k++) {
 						for (int z = 6; z < 9; z++) {
-							if (test != 0 && ((k * 9 + z) != (i * 9 + j)) && (test == puzzle[k * 9 + z])){
-								System.out.println("FALSE_6");
+							if (test != 0 && ((k * 9 + z) != (i * 9 + j))
+									&& (test == puzzle[k * 9 + z])) {
+								// System.out.println("FALSE_6");
 								return false;
-								}
+							}
 						}
 					}
 					break;
-					
-					
-					default: continue;
+
+				default:
+					continue;
 				}
 
-            }
-        }
-        
-        return true;
-    }
-    
-    public void charpuzzle(){
-    	chpuzzle=new char[81];
-    	char[] konvert=new char[9];
-    	for(int i=0;i<9;i++){
-    		konvert[puzzle[i*10]-1]=karakterkeszlet[i];
-    	}
-    	for(int i=0;i<81;i++){
-    		chpuzzle[i]=konvert[puzzle[i]-1];
-    	}
-    }
-    
-    @Override
-    public String toString()
-    {
-        String string= "";
-        for (int i=0; i <9;i++)
-        {
-            for  (int j = 0; j<9;j++)
-            {
-                string = string+puzzle[i*9+j]+" ";
-                if((j+1)%3 == 0) string += "|";
-            }
-            string =string +"\n";
-            if((i+1)%3 == 0) string += "--------------------\n";
-        }
-        return string;
-    } 
-    
-	public void ToltPuzzle(int[] puzzle){
+			}
+		}
+
+		return true;
+	}
+
+	public void charpuzzle() {
+		chpuzzle = new char[81];
+		char[] konvert = new char[9];
+		for (int i = 0; i < 9; i++) {
+			konvert[puzzle[i * 10] - 1] = karakterkeszlet[i];
+		}
+		for (int i = 0; i < 81; i++) {
+			chpuzzle[i] = konvert[puzzle[i] - 1];
+		}
+	}
+
+	@Override
+	public String toString() {
+		String string = "";
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				string = string + puzzle[i * 9 + j] + " ";
+				if ((j + 1) % 3 == 0)
+					string += "|";
+			}
+			string = string + "\n";
+			if ((i + 1) % 3 == 0)
+				string += "--------------------\n";
+		}
+		return string;
+	}
+
+	public void ToltPuzzle(int[] puzzle) {
 		int[] kilenc = new int[9];
-		
-		for(int i = 0; i < 9; i++){
-			kilenc[i] = i+1;
+
+		for (int i = 0; i < 9; i++) {
+			kilenc[i] = i + 1;
 		}
-		
-		for(int sorszam = 0; sorszam < 9; sorszam++){
-			
-	    		Random rand = new Random();
-				int value = rand.nextInt(9);
-				
-				while (kilenc[value] == -1) {
-					value = rand.nextInt(9);
-	    	}
-				puzzle[sorszam] = kilenc[value];
-				kilenc[value] = -1;
+
+		for (int sorszam = 0; sorszam < 9; sorszam++) {
+
+			Random rand = new Random();
+			int value = rand.nextInt(9);
+
+			while (kilenc[value] == -1) {
+				value = rand.nextInt(9);
+			}
+			puzzle[sorszam] = kilenc[value];
+			kilenc[value] = -1;
 		}
-    }
-    
+		for (int sorszam = 9; sorszam < 81; sorszam++) {
+			puzzle[sorszam] = 0;
+		}
+	}
+
 }
